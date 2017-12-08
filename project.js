0569d470e0c087b55f692aa42187e85f786a0f2f
Tim Tolley <Tim@TimsMacBook.local>
2017-12-08 13:28:05 +1300

    plane moves with button push and explodes

--- project.js
+++ project.js
@@ -1,46 +1,84 @@
-document.addEventListener("DOMContentLoaded", function () {
+//document.addEventListener("DOMContentLoaded", function () {
+function start(){
+  var startConsole=document.getElementById("startConsole")
+  startConsole.style.display='none';
+  startConsole.style.visibility='hidden';
+
 
 //set position to zero
-var leftPos=0;
-var topPos=0;
+  var leftPos=0;
+  var topPos=0;
+  var maxScroll=5000;
+  var maxHeight=-300;
+  var minHeight=550;
 
 //attach plane img to plane var
-var plane=document.getElementById("plane")
+  var plane=document.getElementById("plane");
+//create variables for fuel
+  var fuelDiv=document.getElementById("fuel");
+  var fuelGuage=document.getElementById("fuelRemaining");
+
 
 //plane move forward function
+
+function fullAnimation(){
  function planeMove(){
-   leftPos+=5;
+
+   leftPos+=4;
    plane.style.left=leftPos+"px";
-   requestAnimationFrame(planeMove, plane);
+   fuelDiv.style.left=leftPos+"px";
+   if (leftPos==maxScroll){
+
+     plane.style.visibility='hidden';
+     explotion.style.visibility='visible';
+     $('plane').stop();
+     return;
+
+     //window.alert("You ran out of fuel :(");
+
    }
-  function screenScroll(){
-    leftPos+=5;
-    window.scrollLeft(leftPos);
-  }
+ }
+
+  planeMove();
+   requestAnimationFrame(fullAnimation, plane);
+   var fuel = 0;
+   if (leftPos<5000){fuel=5000-leftPos}
+   else {fuel=0}
+   fuelGuage.innerHTML=fuel;
+}
+
+
+
+
+
 //plane up function
-function planeUp(){
+function planeDown(){
   topPos+=5;
   plane.style.top=topPos+"px";
-  requestAnimationFrame(planeMove, plane);
+  if (topPos<=minHeight){
+    return;
+   }
+  requestAnimationFrame(planeUp, plane);
 }
 
-function planeDown(){
-  topPos+=-5;
+function planeUp(){
+  topPos-=5;
   plane.style.top=topPos+"px";
-  requestAnimationFrame(planeMove, plane);
+  if (topPos>=maxheight){
+    window.alert("You safely Landed the Plane!");
+    return;
+   }
+  requestAnimationFrame(planeUp, plane);
 }
+
+
+//call plane move
+fullAnimation();
+
+
+
 
 //keydown function
-document.onKeydown = function checkKey(e){
-  e=e || window.event;
-  if(e.keycode===38){
-    planeUp();
-  }
-  else if (e.keycode===40){
-    planeDown();
-  }
-}
-
 document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 38:
@@ -49,8 +87,10 @@ document.onkeydown = function(e) {
         case 40:
             planeDown();
             break;
-    }
-};
-//call plane move
-planeMove();
-})
+          }
+    };
+  };
+//});
+
+
+//planeMove function is being recalled after every key press making the speed increase each time. hmmmm
